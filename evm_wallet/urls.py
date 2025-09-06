e6ca104940abaf619fb47fb81cf98a018788391e
FollowDragons <followdragons.nft@gmail.com>
2025-09-06 19:21:26 +0200

    Rename auth app to _auth to avoid conflicts with Django's built-in auth

--- evm_wallet/urls.py
+++ evm_wallet/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('auth/', include('auth.urls')),
+    path('auth/', include('_auth.urls')),
 ]
